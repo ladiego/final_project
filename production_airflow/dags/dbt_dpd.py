01d9870690f869a6144338e103d2037da60ca065
Diego <dpd.kerja@gmail.com>
2025-02-14 22:44:54 +0700

    working well

---
+++ production_airflow/dags/dbt_dpd.py
@@ -0,0 +1,41 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+from helper.notify import notify_on_success, notify_on_error, notify_on_retry
+
+default_args = {
+    "owner": "dpd.kerja",
+    "start_date": datetime(2025, 2, 2),
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+    "on_success_callback": notify_on_success,
+    "on_failure_callback": notify_on_error,
+    "on_retry_callback": notify_on_retry,
+}
+
+with DAG(
+    "dbt_running", 
+    default_args=default_args, 
+    schedule_interval="0 3 * * *", 
+    catchup=False,
+    tags=["dbt"],
+    max_active_runs=3,
+    
+    ) as dag:
+
+    dbt_run_prep = BashOperator(
+        task_id="dbt_preparation",
+        bash_command="bash -c docker exec -i dbt_dpd dbt run --project-dir /usr/app/dbt --profiles-dir /usr/app/dbt --select preparation"
+    )
+
+    dbt_run_dimfact = BashOperator(
+        task_id="dbt_dimfact",
+        bash_command="bash -c exec -i dbt_dpd dbt run --project-dir /usr/app/dbt --profiles-dir /usr/app/dbt --select dimfact"
+    )
+
+    dbt_run_marts= BashOperator(
+        task_id="dbt_mart",
+        bash_command="bash -c exec -i dbt_dpd dbt run --project-dir /usr/app/dbt --profiles-dir /usr/app/dbt --select datamart"
+    )
+
+    dbt_run_prep >> dbt_run_dimfact >> dbt_run_marts
